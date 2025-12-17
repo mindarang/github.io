1fa57d5845db6255891966db2c3c1f0d22c2f8c4
kangyeop.lee <kangyeop.lee@linecorp.com>
2025-12-17 15:17:20 +0900

    fix: update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite:{
+        optimizeDeps: {
+            include: ['swiper', 'swiper/modules']
+        },
+        build: {
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        swiper: ['swiper', 'swiper/modules']
+                    }
+                }
+            }
+        }
+    },
+    integrations: [react()]
+});
